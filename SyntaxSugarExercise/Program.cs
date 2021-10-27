3f8265a817d231fcd5d0bc2e8eb54aabc7140fb8
yologan129 <logan128128@gmail.com>
2021-10-27 09:56:11 -0500

    Made Line easier to read

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4; //Explcit Typing
+            //string response; //Explcit Typing
+
+            //if (answer < 9)
+            //{
+             //   response = answer + " is less than nine";
+            //}
+            //else
+           // {
+              //  response = answer + "greater than or equal to nine";
+           // }
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"(answer) is greater than nine";
+        }
+    }
+}
